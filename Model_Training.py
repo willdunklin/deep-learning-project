7d39dc370d5c97eac87ff9ac443adb8b14692ffc
ocasc <orcasciotti@comcast.net>
2020-11-16 17:44:17 -0500

    Added Comments

--- Model_Training.py
+++ Model_Training.py
@@ -10,7 +10,10 @@ from numpy import asarray
 import os
 
 # Data prep
+
+# This is how many images to check each time
 window_size = 16
+
 labels = []
 with open("train.txt") as f:
     for line in f:
@@ -18,20 +21,19 @@ with open("train.txt") as f:
     for i in range(len(labels)):
         labels[i] = float(labels[i][:-2])
 
-labels = labels[window_size:64]
+# This removes the first bunch of labels because we use the last image of the window to determine speed
+labels = labels[window_size:64] # Remove 64 when testing on full dataset
 
 data = []
-for picture in os.listdir("Some_images"):
+for picture in os.listdir("Some_images"): # Some_images only contains 64 images, for testing
     data.append(asarray(Image.open("Some_images/" + picture)))
 
 combined_data = []
-for i in range(len(data)-16):
-    combined_data.append(data[i:i+window_size])
-
-print(len(combined_data))
-print(len(data))
+for i in range(len(data) - 16): # Looping through all but the last 16 images to combine into one array
+    combined_data.append(data[i:i + window_size])
 
 
+# Defining the model
 INPUT_SHAPE = (window_size, 120, 160, 3)
 model2 = Sequential()
 model2.add(Conv3D(32, (3, 3, 3), padding="same", input_shape=INPUT_SHAPE))
@@ -52,7 +54,7 @@ model2.add(Flatten())
 model2.add(Dense(1024, activation="relu"))
 model2.add(Dense(1024, activation="relu"))
 model2.add(Dense(1, activation="relu"))
-
 model2.compile(optimizer="adam", loss="MSE", metrics=["accuracy"])
 
+# Training the model
 model2.fit([combined_data], labels, epochs=1, verbose=1)
