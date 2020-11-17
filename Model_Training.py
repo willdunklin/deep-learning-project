796126310563d71becc6b061de57d6233d4c79a5
ocasc <orcasciotti@comcast.net>
2020-11-16 21:32:49 -0500

    Now saves model and has test and validation splits too

--- Model_Training.py
+++ Model_Training.py
@@ -5,9 +5,12 @@ from keras.models import Sequential
 from keras.optimizers import SGD
 from keras.losses import mean_squared_error
 from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
 from PIL import Image
 from numpy import asarray
 import os
+
+
 
 # Data prep
 
@@ -22,15 +25,20 @@ with open("train.txt") as f:
         labels[i] = float(labels[i][:-2])
 
 # This removes the first bunch of labels because we use the last image of the window to determine speed
-labels = labels[window_size:64] # Remove 64 when testing on full dataset
+labels = labels[window_size:]
 
 data = []
-for picture in os.listdir("Some_images"): # Some_images only contains 64 images, for testing
-    data.append(asarray(Image.open("Some_images/" + picture)))
+folder = "Images_2"
+for picture in os.listdir(folder): # Some_images only contains 64 images, for testing
+    data.append(asarray(Image.open(folder + "/" + picture)))
 
 combined_data = []
 for i in range(len(data) - 16): # Looping through all but the last 16 images to combine into one array
     combined_data.append(data[i:i + window_size])
+
+
+# Split the data
+x_train, x_valid, y_train, y_valid = train_test_split(combined_data, labels, test_size=0.2, shuffle= True)
 
 
 # Defining the model
@@ -57,4 +65,6 @@ model2.add(Dense(1, activation="relu"))
 model2.compile(optimizer="adam", loss="MSE", metrics=["accuracy"])
 
 # Training the model
-model2.fit([combined_data], labels, epochs=1, verbose=1)
+model2.fit([x_train], y_train, epochs=1, batch_size=1 ,verbose=1, validation_split=.2)
+
+model2.save("models")
