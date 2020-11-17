5a0c729f2553f0d18da511ff68a0247e88335aa7
ocasc <orcasciotti@comcast.net>
2020-11-17 00:37:35 -0500

    Late night changes, model saving should work

--- Model_Training.py
+++ Model_Training.py
@@ -24,8 +24,7 @@ with open("train.txt") as f:
     for i in range(len(labels)):
         labels[i] = float(labels[i][:-2])
 
-# This removes the first bunch of labels because we use the last image of the window to determine speed
-labels = labels[window_size:]
+
 
 data = []
 folder = "Images_2"
@@ -36,38 +35,43 @@ combined_data = []
 for i in range(len(data) - 16): # Looping through all but the last 16 images to combine into one array
     combined_data.append(data[i:i + window_size])
 
+# This removes the first bunch of labels because we use the last image of the window to determine speed
+labels = asarray(labels[window_size:])
 
 # Split the data
-x_train, x_valid, y_train, y_valid = train_test_split(combined_data, labels, test_size=0.2, shuffle= True)
-
-
-# Defining the model
-INPUT_SHAPE = (window_size, 120, 160, 3)
-model2 = Sequential()
-model2.add(Conv3D(32, (3, 3, 3), padding="same", input_shape=INPUT_SHAPE))
-model2.add(MaxPool3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
-model2.add(Conv3D(64, (3, 3, 3), padding="same"))
-model2.add(MaxPool3D(pool_size=(2, 2, 2), strides=(1, 2, 2)))
-model2.add(Conv3D(128, (3, 3, 3), padding="same"))
-model2.add(Conv3D(128, (3, 3, 3), padding="same"))
-model2.add(Conv3D(128, (3, 3, 3), padding="same"))
-model2.add(MaxPool3D(pool_size=(2, 2, 2), strides=(2, 2, 2)))
-model2.add(Conv3D(256, (3, 3, 3), padding="same"))
-model2.add(Conv3D(256, (3, 3, 3), padding="same"))
-model2.add(Conv3D(256, (3, 3, 3), padding="same"))
-model2.add(MaxPool3D(pool_size=(2, 2, 2), strides=(2, 2, 2)))
-model2.add(Conv3D(512, (3, 3, 3), padding="same"))
-model2.add(MaxPool3D(pool_size=(2, 2, 2), strides=(2, 2, 2)))
-model2.add(Flatten())
-model2.add(Dense(1024, activation="relu"))
-model2.add(Dense(1024, activation="relu"))
-model2.add(Dense(1, activation="relu"))
-model2.compile(optimizer="adam", loss="MSE", metrics=["accuracy"])
-
+x_train, x_valid, y_train, y_valid = train_test_split(combined_data, labels, test_size=0.2, shuffle= False)
+print(len(x_train), len(y_train))
 try:
-    # Training the model
-    model2.fit([x_train], y_train, epochs=5, batch_size=2 ,verbose=1, validation_split=.2)
+    if "model.keras" in os.listdir(os.getcwd()):
+        # Load from a saved state
+        model2 = keras.models.load_model("model.keras")
+    else:
+        # Defining the model
+        INPUT_SHAPE = (window_size, 120, 160, 3)
+        model2 = Sequential()
+        model2.add(Conv3D(16, (3, 3, 3), padding="same", input_shape=INPUT_SHAPE))
+        model2.add(MaxPool3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
+        model2.add(Conv3D(32, (3, 3, 3), padding="same"))
+        model2.add(MaxPool3D(pool_size=(2, 2, 2), strides=(1, 2, 2)))
+        model2.add(Conv3D(64, (3, 3, 3), padding="same"))
+        model2.add(Conv3D(64, (3, 3, 3), padding="same"))
+        model2.add(Conv3D(64, (3, 3, 3), padding="same"))
+        model2.add(MaxPool3D(pool_size=(2, 2, 2), strides=(2, 2, 2)))
+        model2.add(Conv3D(128, (3, 3, 3), padding="same"))
+        model2.add(Conv3D(128, (3, 3, 3), padding="same"))
+        model2.add(Conv3D(128, (3, 3, 3), padding="same"))
+        model2.add(MaxPool3D(pool_size=(2, 2, 2), strides=(2, 2, 2)))
+        model2.add(Conv3D(256, (3, 3, 3), padding="same"))
+        model2.add(MaxPool3D(pool_size=(2, 2, 2), strides=(2, 2, 2)))
+        model2.add(Flatten())
+        model2.add(Dense(512, activation="relu"))
+        model2.add(Dense(512, activation="relu"))
+        model2.add(Dense(1, activation="relu"))
+        model2.compile(optimizer="adam", loss="MAE", metrics=["accuracy"])
 
-    model2.save("models")
+    # Training the model
+    model2.fit([x_train], y_train, epochs=5, batch_size=2, verbose=1)
+
+    model2.save("model.keras")
 except KeyboardInterrupt:
-    model2.save("models")
+    model2.save("model.keras")
